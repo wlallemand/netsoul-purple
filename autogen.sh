7902da50994b808bfea8a668c7611a02baf576b5
stuyck_J <stuyck_J@f0979099-a728-0410-875a-f67a948257e9>
2007-02-04 15:58:47 +0000

    Installation process updated

--- autogen.sh
+++ autogen.sh
@@ -12,6 +12,15 @@ automake --add-missing --copy
 autoconf || exit;
 automake || exit;
 
-echo "Running ./configure $@"
-echo;
-./configure $@
+NS_DEFAULT_PATH=/usr
+if [ -e ./configure ];then
+    echo "Please enter the installation path of gaim-netsoul : default /usr/lib/gaim"
+    read $NS_PATH
+    if [ "$NS_PATH" == "" ];then
+	NS_PATH=$NS_DEFAULT_PATH
+    fi
+    echo "Gaim-Netsoul will be installed in : $NS_PATH"
+    ./configure --prefix=$NS_PATH
+else
+    echo "Error in configure file generation."
+fi
