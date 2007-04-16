6cd5b86ce4c5b3e791ff3696cb22aaf8e3f0a2ae
stuyck_j <stuyck_j@f0979099-a728-0410-875a-f67a948257e9>
2007-04-16 15:59:42 +0000

    - Update of the autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -13,11 +13,25 @@ autoconf || exit;
 automake || exit;
 
 NS_DEFAULT_PATH=/usr
+
+if [ ! `gaim --version` == "Gaim 2.0.0beta6" ]; then
+    echo "Please install Gaim 2.0.0beta6"
+    exit 1
+fi
+
+
 if [ -e ./configure ];then
-    echo "Please enter the installation path of gaim-netsoul : default /usr/lib/gaim"
-    read $NS_PATH
-    if [ "$NS_PATH" == "" ];then
+    echo "Configuration of the target path"
+    if [ -e /usr/lib/gaim/gaimrc.so ]; then
 	NS_PATH=$NS_DEFAULT_PATH
+    else
+	if [ -e /usr/local/lib/gaim/gaimrc.so ]; then
+	    NS_PATH=$NS_DEFAULT_PATH/local/
+	else
+	    echo "Gaim lib folder not found ( /usr/lib and /usr/local/lib tested) "
+	    echo "Please enter the path where gaim's lib are"
+	    read $NS_PATH
+	fi
     fi
     echo "Gaim-Netsoul will be installed in : $NS_PATH"
     ./configure --prefix=$NS_PATH
